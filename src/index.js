1e889c25ffce9d336b3fa96bd841c7b81ec0a693
Stefan <stefan.mircovici@student.vinci.be>
2021-12-04 13:08:38 +0100

    move files from Domain to Components

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Custom styles
 import './stylesheets/main.css';
 
-import { Router } from './Domain/Router/Router';
-import Navbar from './Domain/Navbar/Navbar';
+import { Router } from './Components/Router/Router';
+import Navbar from './Components/Navbar/Navbar';
 
 // This is the entry point to your app : add all relevant import and custom code
 
