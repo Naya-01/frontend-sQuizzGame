461cf30babe93e7eece2a040525a526faf1f4e38
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2021-12-06 13:04:43 +0100

    backup navbar again

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -43,6 +43,7 @@ const Navbar = () => {
                 e.preventDefault();
                 if (e.target.innerHTML === "Home") {
                     Redirect("/HomePage");
+                    console.log("ici");
                 }
             });
         });
