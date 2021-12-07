f1608567dae31d4d91f5c5388ed266dea6581333
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2021-12-07 18:35:43 +0100

    ajout de la progressBar

--- src/Components/Pages/GamePage.js
+++ src/Components/Pages/GamePage.js
@@ -1,11 +1,7 @@
-let easy=30;
-let medium=20;
-let hard=10;
-
-
+import ProgressBar from "progressbar.js";
+"use strict";
 let myPage = `<div id="page" class="container-fluid">
-        <div id="bar-progress" class="bg-info row">
-        ---------------
+        <div id="bar-progress" class="row">
         </div>
         <div id="couldown" class="bg-success row justify-content-center">
         24h
@@ -30,22 +26,22 @@ let myPage = `<div id="page" class="container-fluid">
 
         <div id="answers" class="container-md text-center">
             <div class="row">
-                <div class="answer p-5 mt-4 border  bg-danger shadow p-3">
+                <div class="answer p-5 mt-4 bg-danger shadow p-3 container" style="width: 70%;">
                     ANSWER 1
                 </div>
             </div>
             <div class="row ">
-                <div class="answer p-5  mt-4 border bg-success shadow p-3">
+                <div class="answer p-5  mt-4 bg-success shadow p-3 container" style="width: 70%;">
                     ANSWER 2
                 </div>
             </div>
             <div class="row ">
-                <div class="answer p-5 mt-4 border bg-danger shadow p-3">
+                <div class="answer p-5 mt-4 bg-danger shadow p-3 container" style="width: 70%;">
                     ANSWER 3
                 </div>
             </div>
             <div class="row ">
-                <div class="answer p-5 mt-5 border bg-danger shadow p-3">
+                <div class="answer p-5 mt-4 bg-danger shadow p-3 container" style="width: 70%;">
                     ANSWER 4
                 </div>
             </div>
@@ -64,9 +60,30 @@ let myPage = `<div id="page" class="container-fluid">
 
     </div>`;
 
+let easy=3000;
+let medium=2000;
+let hard=1000;
+
+
 function GamePage() {
     const myMain = document.querySelector("main");
     myMain.innerHTML = myPage;
+
+    //Bar
+    let divBar = document.getElementById('bar-progress');
+    let bar = new ProgressBar.Line(divBar, {
+        strokeWidth: 4,
+        easing: 'linear',
+        duration: 3000,
+        color: '#FFEA82',
+        svgStyle: {width: '100%', height: '25px'},
+    });
+    let pc =1;
+    bar.set(pc);  // Number from 0.0 to 1.0
+    bar.animate(0);
+
+
+
 }
 
 export {GamePage};
