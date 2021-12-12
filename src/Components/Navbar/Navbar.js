5c229c429c12de7911eb607f907729eeaec0fe92
Stefan <stefan.mircovici@student.vinci.be>
2021-12-12 20:05:09 +0100

    add - Mon profil tab on navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -38,16 +38,19 @@ const Navbar = async () => {
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#" data-page="/CreateQuizz">Créer un quizz</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#" data-page="/Profil/MyProfil">Mon profil</a>
+                </li>
                 `;
                 if (user.is_admin) {
                     navbar += `
                     <li class="nav-item">
-                        <a class="nav-link" href="#" data-page="/PanelAdmin">Gestion administrative</a>
+                        <a class="nav-link active" href="#" data-page="/PanelAdmin">Gestion administrative</a>
                     </li>`;
                 }
                 navbar += `
                 <li class="nav-item">
-                    <a class="nav-link" href="#" data-page="/logout">Se deconnecter</a>
+                    <a class="nav-link active" href="#" data-page="/logout">Se deconnecter</a>
                 </li>
             </ul>
         </div>
